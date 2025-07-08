ce8a3c159feb7f63fc3468edbd4649aad04f1f50
Hakan Bilgin <hbi99@hotmail.com>
2025-07-08 21:19:06 +0200

    added rewards pyro

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -4,8 +4,8 @@ let Test = {
 		// APP.dispatch({ type: "show-view", arg: "game" });
 		// APP.dispatch({ type: "open-dialog", arg: "reward", addClass: "completed" });
 
-		setTimeout(() => APP.dispatch({ type: "show-view", arg: "game" }), 1000);
-		setTimeout(() => APP.dispatch({ type: "show-view", arg: "start" }), 4000);
+		// setTimeout(() => APP.dispatch({ type: "show-view", arg: "game" }), 1000);
+		// setTimeout(() => APP.dispatch({ type: "show-view", arg: "start" }), 4000);
 
 		// setTimeout(() => {
 		// 	APP.content.find(".marker-chest").cssSequence("on", "transitionend", el => {
